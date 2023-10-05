60ea940fc486e19bb793a6ba46b64339f924a841
mubashir malik <malikmubashir601@gmail.com>
2023-10-05 11:35:36 -0700

    create van detail page and fetch van detail

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -3,7 +3,26 @@ import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
     const params = useParams()
-    console.log(params)
-    
-    return <h1>Van detail page goes here</h1>
+    const [van, setVan] = React.useState(null)
+
+    React.useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => setVan(data.vans))
+    }, [params.id])
+
+    return (
+        <div className="van-detail-container">
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} />
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
+    )
 }
