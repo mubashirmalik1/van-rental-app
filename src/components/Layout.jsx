1b77adc7fe492e7ccfcbd5bf5f2c8eb486aeba9d
mubashir malik <malikmubashir601@gmail.com>
2023-10-08 11:03:12 -0700

    add footer with styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
+import Footer from "./Footer"
+
 export default function Layout() {
-    /**
-     * Challenge: get the Header working again
-     * outlet is same as section or yield in laravel
-     */
     return (
-        <>
+        <div className="site-wrapper">
             <Header />
-            <Outlet />
-        </>
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
     )
 }
